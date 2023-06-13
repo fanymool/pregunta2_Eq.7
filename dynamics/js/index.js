465153e3285cb3da2b928e6a2fa41426e6bda070
Daniel Rodríguez <87953599+danibutnotdani@users.noreply.github.com>
2023-06-12 20:10:51 -0600

    ProgresoDani

--- dynamics/js/index.js
+++ dynamics/js/index.js
@@ -1,87 +1,94 @@
 let link = document.createElement('link');
 link.rel = 'stylesheet';
-link.href = './statics/styles/index.css';
+link.href = './statics/styles/principal.css';
 document.head.appendChild(link);
-
-
 function Victoria() {
 
 }
 function Respuesta() {
 
 }
-function Pregunta() {
-
+function Pregunta(materia) {
+    console.log("La materia seleccionada es: " + materia); //Funciona
 }
 function Juego(player1, player2) {
-    //Esta es para elmiminar a todos los hijos del contenedor y limpiar la pantalla aaa
+    // Esta es para eliminar a todos los hijos del contenedor y limpiar la pantalla
     const contenedor = document.body;
     while (contenedor.firstChild) {
         contenedor.removeChild(contenedor.firstChild);
     }
-    
+
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
     link.href = './statics/styles/juego.css';
     document.head.appendChild(link);
-    
+
     const header = document.createElement("header");
     const h1 = document.createElement("h1");
-    h1.textContent = "Turno de: " + player1; // Aquí supongo que inicia el jugador 1
+    h1.textContent = "Turno de: " + player1;
     header.appendChild(h1);
     contenedor.appendChild(header);
-    
+
     const main = document.createElement("main");
 
-    const ruletaSection = document.createElement("section");
-    ruletaSection.id = "ruleta";
-
-    const p = document.createElement("p");
-    p.textContent = "random ruleta";
-
-    const button = document.createElement("button");
-    button.id = "girar";
-    button.textContent = "Girar";
-
-    ruletaSection.appendChild(p);
-    ruletaSection.appendChild(button);
-    
     const player1Section = document.createElement("section");
     player1Section.id = "player1";
-
     const h2Player1 = document.createElement("h2");
     h2Player1.textContent = "Player 1: " + player1;
-
     const h3Player1 = document.createElement("h3");
     h3Player1.textContent = "Materias obtenidas";
-
     const pPlayer1 = document.createElement("p");
     pPlayer1.textContent = "Matemáticas: Obtenida";
-
     player1Section.appendChild(h2Player1);
     player1Section.appendChild(h3Player1);
     player1Section.appendChild(pPlayer1);
-    
+
+    const ruletaSection = document.createElement("section");
+    ruletaSection.id = "ruleta";
+    const p = document.createElement("p");
+    p.textContent = "random ruleta";
+    const materias = ["Matemáticas", "Física", "Química", "Psicología", "Literatura", "Computación"];
+    let timerId;
+    const button = document.createElement("button");
+    button.id = "girar";
+    button.textContent = "Girar";
+    button.addEventListener('click', function(){
+        timerId = setInterval(function() {
+            button.disabled = true;
+            materiaSeleccionada = materias[Math.floor(Math.random() * materias.length)];
+            p.textContent = "Materia seleccionada: " + "\n" + materiaSeleccionada;
+        }, 100);
+        setTimeout(function() {
+            clearInterval(timerId);
+            button.disabled = false;
+            Pregunta(materiaSeleccionada);
+        }, 5000);
+    });
+
+    ruletaSection.appendChild(p);
+    ruletaSection.appendChild(button);
+
     const player2Section = document.createElement("section");
     player2Section.id = "player2";
-
     const h2Player2 = document.createElement("h2");
     h2Player2.textContent = "Player 2: " + player2;
-
     const h3Player2 = document.createElement("h3");
     h3Player2.textContent = "Materias obtenidas";
-
     const pPlayer2 = document.createElement("p");
     pPlayer2.textContent = "Historia: Obtenida";
-
     player2Section.appendChild(h2Player2);
     player2Section.appendChild(h3Player2);
     player2Section.appendChild(pPlayer2);
 
-    main.appendChild(ruletaSection);
-    main.appendChild(player1Section);
-    main.appendChild(player2Section);
+    //Necesario para el acomodo de las partes del juego
+    //NO MODIFICAR
+    main.appendChild(player1Section);  // Primero player1Section
+    main.appendChild(ruletaSection);  // Luego ruletaSection
+    main.appendChild(player2Section);  // Finalmente player2Section
 
     contenedor.appendChild(main);
 }
+
 
 function iniciar() {
     const contenedor = document.body;
@@ -156,7 +163,7 @@ function iniciar() {
         if (player1 === "" || player2 === "") {
             alert("Debes ingresar un nombre para cada jugador");
         } else {
-           Juego(player1, player2); 
+            Juego(player1, player2);
         }
     });
 
